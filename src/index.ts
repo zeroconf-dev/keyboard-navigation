24f6100b5b502488a1128d51de141bbf27b940dd
Brian K. Christensen <briankchristensen@gmail.com>
2018-12-20 09:58:41 +0100

    Grid: export the grid component in index file

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './FieldNavigation';
 export * from './TabRegistry';
 export * from './components/Field';
 export * from './components/Focuser';
+export * from './components/Grid';
 export * from './components/Section';
 export * from './components/Tabbable';
 export * from './components/TabBoundary';
