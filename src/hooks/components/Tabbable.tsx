9a001582117f8f24a48815e42293ac9595ea51a4
Brian K. Christensen <briankchristensen@gmail.com>
2019-01-10 12:02:53 +0100

    Hooks: implement hook version of Tabbables

---
+++ src/hooks/components/Tabbable.tsx
@@ -0,0 +1,114 @@
+import React, { useCallback, useRef } from 'react';
+import { useFocusable } from '../useFocusable';
+
+export interface TabbableProps {
+    name: string;
+}
+
+type ButtonProps = JSX.IntrinsicElements['button'] & TabbableProps;
+export const Button = React.forwardRef((props: ButtonProps, ref: React.Ref<HTMLButtonElement>) => {
+    const buttonRef = useRef<HTMLButtonElement>(null);
+    const disabled = props.disabled || false;
+
+    const focus = useCallback(
+        () => {
+            if (buttonRef.current == null || disabled) {
+                return false;
+            }
+            buttonRef.current.focus();
+            return true;
+        },
+        [props.name, disabled],
+    );
+
+    if (typeof ref === 'function') {
+        ref(buttonRef.current);
+    } else if (typeof ref === 'object' && ref != null) {
+        (ref as React.MutableRefObject<HTMLButtonElement | null>).current = buttonRef.current;
+    }
+
+    useFocusable(props.name, focus);
+
+    return <button {...props} ref={buttonRef} />;
+});
+
+type InputProps = JSX.IntrinsicElements['input'] & TabbableProps;
+export const Input = React.forwardRef((props: InputProps, ref: React.Ref<HTMLInputElement>) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const disabled = props.disabled || false;
+
+    const focus = useCallback(
+        () => {
+            if (inputRef.current == null || disabled) {
+                return false;
+            }
+            inputRef.current.focus();
+            return true;
+        },
+        [props.name, disabled],
+    );
+
+    if (typeof ref === 'function') {
+        ref(inputRef.current);
+    } else if (typeof ref === 'object' && ref != null) {
+        (ref as React.MutableRefObject<HTMLInputElement | null>).current = inputRef.current;
+    }
+
+    useFocusable(props.name, focus);
+
+    return <input {...props} ref={inputRef} />;
+});
+
+type SelectProps = JSX.IntrinsicElements['select'] & TabbableProps;
+export const Select = React.forwardRef((props: SelectProps, ref: React.Ref<HTMLSelectElement>) => {
+    const selectRef = useRef<HTMLSelectElement>(null);
+    const disabled = props.disabled || false;
+
+    const focus = useCallback(
+        () => {
+            if (selectRef.current == null || disabled) {
+                return false;
+            }
+            selectRef.current.focus();
+            return true;
+        },
+        [props.name, disabled],
+    );
+
+    if (typeof ref === 'function') {
+        ref(selectRef.current);
+    } else if (typeof ref === 'object' && ref != null) {
+        (ref as React.MutableRefObject<HTMLSelectElement | null>).current = selectRef.current;
+    }
+
+    useFocusable(props.name, focus);
+
+    return <select {...props} ref={selectRef} />;
+});
+
+type TextAreaProps = JSX.IntrinsicElements['textarea'] & TabbableProps;
+export const TextArea = React.forwardRef((props: TextAreaProps, ref: React.Ref<HTMLTextAreaElement>) => {
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
+    const disabled = props.disabled || false;
+
+    const focus = useCallback(
+        () => {
+            if (textAreaRef.current == null || disabled) {
+                return false;
+            }
+            textAreaRef.current.focus();
+            return true;
+        },
+        [props.name, disabled],
+    );
+
+    if (typeof ref === 'function') {
+        ref(textAreaRef.current);
+    } else if (typeof ref === 'object' && ref != null) {
+        (ref as React.MutableRefObject<HTMLTextAreaElement | null>).current = textAreaRef.current;
+    }
+
+    useFocusable(props.name, focus);
+
+    return <textarea {...props} ref={textAreaRef} />;
+});
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
+export { Field, EditStartHandler, EditorRenderer, Props as FieldProps, SubmitHandler } from './components/Field';
 export {
     ArrowKey,
     ArrowKeyHandler,
@@ -12,10 +13,13 @@ export {
     NavigationKeyHandler,
     Props as FocuserProps,
 } from './components/Focuser';
-export { Field, EditStartHandler, EditorRenderer, Props as FieldProps, SubmitHandler } from './components/Field';
 export { Grid, Props as GridProps } from './components/Grid';
 export { Section, Props as SectionProps } from './components/Section';
+export { Button, Input, Select, TextArea } from './components/Tabbable';
 export { TabBoundary, Props as TabBoundaryProps } from './components/TabBoundary';
 
 export { useFocusable } from './useFocusable';
 export { useTabRegistry } from './useTabRegistry';
+
+export * from '../FieldNavigation';
+export * from '../TabRegistry';
