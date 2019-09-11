df9c7e757dc464c1659b62222302ddf908539b81
Brian K. Christensen <briankchristensen@gmail.com>
2019-09-11 21:13:24 +0200

    CI: setting up CircleCI

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
             tsConfig: 'tsconfig.test.json',
         },
     },
+    coverageDirectory: '<rootDir>/reports/coverage',
     moduleDirectories: ['node_modules'],
     moduleNameMapper: {
         '@zeroconf/keyboard-navigation$': '<rootDir>/src/index',
@@ -12,5 +13,6 @@ module.exports = {
     modulePathIgnorePatterns: ['<rootDir>/build','<rootDir>/.docz','<rootDir>/package'],
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
     preset: 'ts-jest',
+    reporters: ['default', 'jest-junit'],
     testPathIgnorePatterns: ['/node_modules/', '/__helpers__/'],
 };
