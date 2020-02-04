5d49ce14a49566364518f262d66aa6b166118151
Brian K. Christensen <briankchristensen@gmail.com>
2020-02-04 01:25:53 +0100

    Hotkey: better implementation of legend

--- src/hotkeys/components/HotkeyLegend.tsx
+++ src/hotkeys/components/HotkeyLegend.tsx
@@ -1,4 +1,5 @@
 import { useHotkeyRegistry } from '@zeroconf/keyboard-navigation/hotkeys/hooks/useHotkeyRegistry';
+import { HotkeyRegistry } from '@zeroconf/keyboard-navigation/hotkeys/HotkeyRegistry';
 import { Hotkey } from '@zeroconf/keyboard-navigation/hotkeys/parser';
 import * as React from 'react';
 import { useEffect, useState } from 'react';
@@ -10,8 +11,11 @@ interface HotkeyLegendProps {
     renderHotkey: (hotkey: Hotkey) => JSX.Element;
 }
 export const HotkeyLegend: React.FC<HotkeyLegendProps> = props => {
-    const context = useHotkeyRegistry();
-    const [, setUpdater] = useState(0);
-    useEffect(() => context.subscribe(() => setUpdater(s => s + 1)), [context]);
-    return <>{Array.from(context.iterHotkeys()).map(hotkey => props.renderHotkey(hotkey))}</>;
+    const globalRegistry = useHotkeyRegistry().global;
+    const [registry, setRegistry] = useState<HotkeyRegistry | null>(globalRegistry);
+    useEffect(() => globalRegistry.subscribe(reg => setRegistry(reg)), [globalRegistry]);
+
+    return registry == null ? null : (
+        <> {Array.from(registry.iterHotkeys()).map(hotkey => props.renderHotkey(hotkey))}</>
+    );
 };
